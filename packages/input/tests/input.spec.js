b0d124c44e70afd0d2ed8668af3690b8a58665cd
Peter Shershov <stelaviv@gmail.com>
2018-09-23 02:04:47 +0300

    linting support

--- packages/input/tests/input.spec.js
+++ packages/input/tests/input.spec.js
@@ -4,8 +4,8 @@ import renderer from "react-test-renderer";
 import Input from "../input";
 
 describe("Input component", () => {
-	test("Input changes style when active", () => {
-		const cmp = renderer.create(<Input value="TEST" />);
-		expect(cmp).toMatchSnapshot();
-	})
-})
+  test("Input changes style when active", () => {
+    const cmp = renderer.create(<Input value="TEST" />);
+    expect(cmp).toMatchSnapshot();
+  });
+});
