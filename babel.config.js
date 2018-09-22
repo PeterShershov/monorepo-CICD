2fe7f07c6f12e57186d24fa0ff92c12ebb761fed
Peter Shershov <stelaviv@gmail.com>
2018-09-23 02:45:22 +0300

    eslint script

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	presets: ["@babel/preset-env", "@babel/preset-react"],
-	plugins: [
-		"transform-class-properties",
-		"@babel/plugin-syntax-object-rest-spread",
-		"css-modules-transform"
-	]
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+    "transform-class-properties",
+    "@babel/plugin-syntax-object-rest-spread",
+    "css-modules-transform"
+  ]
 };
