6861d2e824c8935d3f43da748d9881ae52942108
Peter Shershov <stelaviv@gmail.com>
2018-09-23 04:13:29 +0300

    test test

--- packages/button/tests/button.test.js
+++ packages/button/tests/button.test.js
@@ -5,11 +5,6 @@ import { shallow } from "enzyme";
 import Button from "../src/button";
 
 describe("Button component", () => {
-  test("Button renders children", () => {
-    const cmp = renderer.create(<Button>TEST</Button>);
-    expect(cmp).toMatchSnapshot();
-  });
-
   test("Button changes style onClick", () => {
     const cmp = shallow(<Button />);
     expect(cmp.hasClass("button_active")).toEqual(false);
